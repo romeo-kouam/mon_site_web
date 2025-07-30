bf120f2e3dbdcb9899b97bcffc37ab16a6702241
romeo-kouam <kouameromeokouame@gmail.com>
2025-07-30 15:17:13 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// Les informations de connexion
+
+define('DB_HOST', 'localhost');         // Adresse du serveur MySQL
+define('DB_NAME', 'etat_civil');  // Nom de la base de données
+define('DB_USER', 'root');              // Nom d'utilisateur MySQL
+define('DB_PASSWORD', '');              // Mot de passe
+
+?>
